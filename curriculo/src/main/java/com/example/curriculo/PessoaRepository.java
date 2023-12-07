9bb29d4af942f5a196eb7b57abbf68a62cd732a4
Maynan Taíze <107214632+maynantaize@users.noreply.github.com>
2023-12-07 01:21:57 +0000

    build-sucess

--- curriculo/src/main/java/com/example/curriculo/PessoaRepository.java
+++ curriculo/src/main/java/com/example/curriculo/PessoaRepository.java
@@ -3,7 +3,7 @@ package com.example.curriculo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.example.curriculo.model.Pessoa; // Substitua pelo caminho correto do seu modelo Pessoa
+import com.example.curriculo.Pessoa; // Substitua pelo caminho correto do seu modelo Pessoa
 
 @Repository
 public interface PessoaRepository extends JpaRepository<Pessoa, Long> {
